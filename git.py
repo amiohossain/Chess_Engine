34df52fe17f649ea7d861525a6c9a64c2d042a45
DarkDevilAmio <amiohossain234@gmail.com>
2025-08-19 00:12:48 +0600

    Updated Time : <class 'datetime.datetime'>

--- git.py
+++ git.py
@@ -8,7 +8,7 @@ def run_command(command):
         print(result.stderr)
 
 def main():
-    commit_message = f"Updated Time : {dt.datetime}"
+    commit_message = f"Updated Time : {str(dt.datetime)}"
     run_command("git add .")
     run_command(f'git commit -m "{commit_message}"')
     run_command("git push")
