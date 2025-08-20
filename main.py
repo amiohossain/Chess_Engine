0dbb47da21e15a575a3181ec1eb90cb385aeef20
DarkDevilAmio <amiohossain234@gmail.com>
2025-08-20 07:51:59 +0600

    Updated Time : 2025-08-20 07:51:58.907881

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ width = height = 512
 dimension = 8
 sq_size = height//dimension
 max_fps = 15
+animate = True
 images = {} #Stores Image
 sqSelected = () #Selected Square
 sqClicked = [] 
 
+gameOver = False
 
 #Image Loader
 def load_img():
@@ -17,9 +19,9 @@ def load_img():
         images[str(piece)] = p.transform.scale(p.image.load(f"./images/{str(piece)}.png") , (sq_size , sq_size))
         
 def main():
-    global sqSelected ,sqClicked
+    global sqSelected ,sqClicked, animate, gameOver
     
-    #Screen Initializer 
+    #scrn Initializer 
     p.init()
     scrn = p.display.set_mode((width , height))
     clk = p.time.Clock()
@@ -38,50 +40,76 @@ def main():
             if e.type == p.QUIT:
                 running = False
             elif e.type == p.MOUSEBUTTONDOWN:
-                loc = p.mouse.get_pos()
-                row = loc[1]//sq_size
-                col = loc[0]//sq_size
-                if sqSelected == (row , col):
-                    sqSelected = ()
-                    sqClicked = []
-                else:
-                    sqSelected = (row , col)
-                    sqClicked.append(sqSelected)
-                if len(sqClicked) == 2:
-                    move = engine.Move(sqClicked[0] , sqClicked[1] , gs.board)
-                    for i in range(len(validMoves)):
-                        if move == validMoves[i]:
-                            gs.makeMove(validMoves[i])
-                            moveMade = True
-                            sqSelected = ()
-                            sqClicked = []
-                            print(move.getChessNotation())
-                    if not moveMade:
-                        sqClicked = [sqSelected] 
+                if not gameOver:
+                    loc = p.mouse.get_pos()
+                    row = loc[1]//sq_size
+                    col = loc[0]//sq_size
+                    if sqSelected == (row , col):
+                        sqSelected = ()
+                        sqClicked = []
+                    else:
+                        sqSelected = (row , col)
+                        sqClicked.append(sqSelected)
+                    if len(sqClicked) == 2:
+                        move = engine.Move(sqClicked[0] , sqClicked[1] , gs.board)
+                        for i in range(len(validMoves)):
+                            if move == validMoves[i]:
+                                gs.makeMove(validMoves[i])
+                                moveMade = True
+                                sqSelected = ()
+                                sqClicked = []
+                                print(move.getChessNotation())
+                        if not moveMade:
+                            sqClicked = [sqSelected] 
             elif e.type == p.KEYDOWN :
-                if e.key == p.K_z:
+                if e.key == p.K_z :
                     gs.undoMove()
                     moveMade = True
+                    animate = False
+                elif e.key == p.K_r :
+                    gs = engine.gameState()
+                    validMoves = gs.getValidMoves()
+                    sqSelected = ()
+                    sqClicked = [] 
+                    moveMade = False
+                    animate = True
+                    
         if moveMade :
+            if animate:
+                animateMove(gs.moveLog[-1], scrn, gs.board, clk)
             validMoves = gs.getValidMoves()
             moveMade = False
+            animate = True
+            
         drawGameState(scrn , gs, validMoves, sqSelected)
+        
+        if gs.checkMate:
+            gameOver = True
+            if gs.whiteToMove:
+                drawText(scrn , "Black wins by Check Mate")
+            else:
+                drawText(scrn , "White wins by Check Mate")
+                
+        elif gs.staleMate:
+            gameOver = True
+            drawText(scrn , "Stalemate")
+                
         clk.tick(max_fps)
         p.display.flip()
         
         
-def highlightSquares(screen , gs, validMoves, sqSelected):
+def highlightSquares(scrn , gs, validMoves, sqSelected):
     if sqSelected != ():
         r,c = sqSelected
-        if (gs.whiteToMove and (1 <= gs.board[r][c] <= 6)) or ((not gs.whiteToMove) and (7 <= gs.boad[r][c] <= 12)):
+        if (gs.whiteToMove and (1 <= gs.board[r][c] <= 6)) or ((not gs.whiteToMove) and (7 <= gs.board[r][c] <= 12)):
             s = p.Surface((sq_size,sq_size))
             s.set_alpha(100)
             s.fill(p.Color("blue"))
-            screen.blit(s, (c*sq_size,r*sq_size))
+            scrn.blit(s, (c*sq_size,r*sq_size))
             s.fill(p.Color("yellow"))
             for move in validMoves:
                 if move.startRow == r and move.startCol == c:
-                    screen.blit(s, (move.endCol*sq_size, move.endRow*sq_size))
+                    scrn.blit(s, (move.endCol*sq_size, move.endRow*sq_size))
                     
         
 def drawGameState(scrn , gs, validMoves, sqSelected):
@@ -90,6 +118,7 @@ def drawGameState(scrn , gs, validMoves, sqSelected):
    drawPiece(scrn,gs.board)
    
 def drawBoard(scrn):
+    global colors
     colors = [p.Color("white") , p.Color("teal")]
     for r in range(dimension):
         for c in range(dimension):
@@ -104,5 +133,33 @@ def drawPiece(scrn , board):
             if piece != 0:
                 scrn.blit(images[str(piece)] , p.Rect(c*sq_size , r*sq_size , sq_size , sq_size))
 
+
+def animateMove(move, scrn, board, clock):
+    global colors
+    dR = move.endRow - move.startRow
+    dC = move.endCol - move.startCol
+    framePerSq = 10
+    frameCnt = (abs(dR)+abs(dC))*framePerSq
+    for frame in range(frameCnt+1):
+        r, c = (move.startRow + dR*frame/frameCnt, move.startCol + dC*frame/frameCnt)
+        drawBoard(scrn)
+        drawPiece(scrn, board)
+        color = colors[(move.endRow + move.endCol)%2]
+        endSquare = p.Rect(move.endCol*sq_size, move.endRow*sq_size, sq_size, sq_size)
+        p.draw.rect(scrn, color, endSquare)
+        if move.captured_piece != 0:
+            scrn.blit(images[str(move.moved_piece)], endSquare)
+        scrn.blit(images[str(move.moved_piece)], p.Rect(c*sq_size, r*sq_size, sq_size, sq_size))
+        p.display.flip()
+        clock.tick(60)
+        
+def drawText(scrn, text):
+    font = p.font.SysFont("Helvitca", 32, True, False)
+    textObject = font.render(text, 0, p.Color("gray"))
+    textLocation = p.Rect(0, 0, width, height).move(width/2 - textObject.get_width()/2, height/2 - textObject.get_height()/2)
+    scrn.blit(textObject, textLocation)
+    textObject = font.render(text, 0, p.Color("Black"))
+    scrn.blit(textObject, textLocation.move(2,2))
+    
 if __name__ == "__main__":
     main()
